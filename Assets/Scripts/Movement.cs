57b7cd082b65bfccda3ee6b3281ea5beca090ec3
Alfred <59073967+slimer37@users.noreply.github.com>
2023-02-03 20:22:09 -0800

    Basic movement/camera look

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,41 +3,44 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] InputAction _move;
-    [SerializeField] InputAction _mouseDelta;
     [SerializeField] CharacterController _controller;
     [SerializeField] Transform _cam;
     [SerializeField] float _sensitivity;
     [SerializeField] float _clamp;
+    [SerializeField] float _speed;
 
-    float camEuler;
+    float _camEuler;
+
+    Controls _controls;
 
     void Awake()
     {
-        _move.Enable();
-        _mouseDelta.Enable();
+        _controls = new Controls();
 
-        _mouseDelta.performed += Look;
+        _controls.Enable();
 
-        camEuler = _cam.localEulerAngles.x;
+        _controls.Player.Look.performed += Look;
+
+        _camEuler = _cam.localEulerAngles.x;
     }
 
     private void Look(InputAction.CallbackContext obj)
     {
         var delta = _sensitivity * Time.deltaTime * obj.ReadValue<Vector2>();
 
-        camEuler -= delta.y;
+        _camEuler -= delta.y;
 
-        camEuler = Mathf.Clamp(camEuler, -_clamp, _clamp);
+        _camEuler = Mathf.Clamp(_camEuler, -_clamp, _clamp);
 
         transform.Rotate(Vector3.up * delta.x);
 
-        _cam.localEulerAngles = Vector3.right * camEuler;
+        _cam.localEulerAngles = Vector3.right * _camEuler;
     }
 
     void Update()
     {
-        var input = _move.ReadValue<Vector2>();
-        _controller.Move(transform.TransformDirection(input));
+        var input = _controls.Player.Move.ReadValue<Vector2>();
+        var movement = new Vector3(input.x, 0, input.y) * _speed * Time.deltaTime;
+        _controller.Move(transform.TransformDirection(movement));
     }
 }
