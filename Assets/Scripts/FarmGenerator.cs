6e244ae51658feaa7e1985a04fcfd75242d074ff
Alfred <59073967+slimer37@users.noreply.github.com>
2023-02-04 14:43:12 -0800

    Chance of no wheat + random offsets

--- Assets/Scripts/FarmGenerator.cs
+++ Assets/Scripts/FarmGenerator.cs
@@ -10,7 +10,9 @@ public class FarmGenerator : MonoBehaviour
     [SerializeField] float rayOffset;
     [SerializeField] Vector3 offset;
     [SerializeField] float gizmoSize;
+    [SerializeField] float chanceNoWheat;
     [SerializeField] Collider ground;
+    [SerializeField] Vector3 _randomOffset;
 
     void OnDrawGizmosSelected()
     {
@@ -37,12 +39,19 @@ public class FarmGenerator : MonoBehaviour
         {
             for (int y = 0; y < gridY; y++)
             {
+                if (Random.value < chanceNoWheat)
+                {
+                    continue;
+                }
+
                 var wheat = wheats[Random.Range(0, wheats.Length)];
                 var clone = Instantiate(wheat, transform);
 
                 var pos = transform.TransformPoint(new Vector3(x * unit, rayOffset, y * unit) + offset);
                 ground.Raycast(new Ray(pos, Vector3.down), out var hit, 100);
                 pos = hit.point + Vector3.up * floorOffset;
+
+                pos += (Random.value * 2 - 1) * _randomOffset;
 
                 clone.transform.position = pos;
             }
