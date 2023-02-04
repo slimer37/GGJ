4f97feb99dae7f22aae8d6217ae980ae2c6eed29
Alfred <59073967+slimer37@users.noreply.github.com>
2023-02-04 11:55:27 -0800

    Combine animation

--- Assets/Scripts/CombineAnimator.cs
+++ Assets/Scripts/CombineAnimator.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class CombineAnimator : MonoBehaviour
@@ -5,6 +6,14 @@ public class CombineAnimator : MonoBehaviour
     [SerializeField] float _rotationSpeed;
     [SerializeField] Transform _cylinder;
     [SerializeField] Transform[] rakes;
+    [SerializeField] Transform _body;
+    [SerializeField] float _shakeStrength;
+    [SerializeField] int _vibrato;
+
+    void Awake()
+    {
+        _body.DOShakeRotation(5, _shakeStrength, _vibrato, 90, false, ShakeRandomnessMode.Harmonic).SetLoops(-1);
+    }
 
     void Update()
     {
