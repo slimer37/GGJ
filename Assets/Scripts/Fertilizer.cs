c39a4017ce80c59713d1d3ea43bc713fe514f042
Alfred <59073967+slimer37@users.noreply.github.com>
2023-02-05 13:38:02 -0800

    Fertilizer float animation

--- Assets/Scripts/Fertilizer.cs
+++ Assets/Scripts/Fertilizer.cs
@@ -4,8 +4,12 @@ public class Fertilizer : MonoBehaviour
 {
     [SerializeField] float points;
     [SerializeField] float heal;
+    [SerializeField] float floatAmplitude;
+    [SerializeField] float floatFrequency;
     [SerializeField] Vector3 spawnCenter;
     [SerializeField] Vector2 spawnArea;
+
+    Vector3 basePosition;
 
     void OnDrawGizmosSelected()
     {
@@ -32,12 +36,19 @@ public class Fertilizer : MonoBehaviour
         Score.Instance.Value += points;
     }
 
+    void Update()
+    {
+        transform.position = basePosition + Vector3.up * (Mathf.Sin(Time.time * floatFrequency) * floatAmplitude + 1);
+    }
+
     float RandomRange(float range) => (Random.value * 2 - 1) * range;
 
     void Teleport()
     {
         var pos = new Vector3(RandomRange(spawnArea.x / 2), 0, RandomRange(spawnArea.y / 2));
 
-        transform.SetPositionAndRotation(spawnCenter + pos, Quaternion.Euler(Vector3.up * Random.value * 360));
+        basePosition = spawnCenter + pos;
+
+        transform.SetPositionAndRotation(basePosition, Quaternion.Euler(Vector3.up * Random.value * 360));
     }
 }
