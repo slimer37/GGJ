5f71d01b05b82b3c788120e25f9b2ff61bfa568d
Alfred <59073967+slimer37@users.noreply.github.com>
2023-02-05 13:31:46 -0800

    Random fertilizer rotation and starting position

--- Assets/Scripts/Fertilizer.cs
+++ Assets/Scripts/Fertilizer.cs
@@ -12,14 +12,24 @@ public class Fertilizer : MonoBehaviour
         Gizmos.DrawWireCube(spawnCenter, new Vector3(spawnArea.x, 0, spawnArea.y));
     }
 
+    void Awake()
+    {
+        Teleport();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Health.Instance.Heal(heal);
-            Teleport();
-            Score.Instance.Value += points;
+            Consume();
         }
+    }
+
+    void Consume()
+    {
+        Health.Instance.Heal(heal);
+        Teleport();
+        Score.Instance.Value += points;
     }
 
     float RandomRange(float range) => (Random.value * 2 - 1) * range;
@@ -28,6 +38,6 @@ public class Fertilizer : MonoBehaviour
     {
         var pos = new Vector3(RandomRange(spawnArea.x / 2), 0, RandomRange(spawnArea.y / 2));
 
-        transform.position = spawnCenter + pos;
+        transform.SetPositionAndRotation(spawnCenter + pos, Quaternion.Euler(Vector3.up * Random.value * 360));
     }
 }
