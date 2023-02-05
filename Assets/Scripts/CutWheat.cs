33e9408dc3e669092882909df4de8ab9162d8e50
Alfred <59073967+slimer37@users.noreply.github.com>
2023-02-05 10:01:36 -0800

    Regrowing wheat

--- Assets/Scripts/CutWheat.cs
+++ Assets/Scripts/CutWheat.cs
@@ -2,11 +2,44 @@ using UnityEngine;
 
 public class CutWheat : MonoBehaviour
 {
+    Collider col;
+
+    float time;
+
+    const float GrowTime = 30;
+    const float GrowDelay = 10;
+    const float GrownThreshold = 25;
+
+    void Awake()
+    {
+        col = GetComponent<Collider>();
+
+        time = GrowTime + GrowDelay + 1;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Kill"))
         {
-            Destroy(gameObject);
+            transform.localScale = Vector3.zero;
+            col.enabled = false;
+            time = 0;
+        }
+    }
+
+    void Update()
+    {
+        if (time > GrowTime + GrowDelay) return;
+
+        time += Time.deltaTime;
+
+        if (time < GrowDelay) return;
+
+        transform.localScale = Vector3.one * ((time - GrowDelay) / GrowTime);
+
+        if (time > GrowDelay + GrownThreshold)
+        {
+            col.enabled = true;
         }
     }
 }
